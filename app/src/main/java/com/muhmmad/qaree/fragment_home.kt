54db19eee0930ef4740241fde8b59853e665e92a
Nourhanmohamed303 <nour7ann@gmail.com>
2024-02-18 17:50:50 +0200

    forget_password

---
+++ app/src/main/java/com/muhmmad/qaree/fragment_home.kt
@@ -0,0 +1,11 @@
+package com.muhmmad.qaree
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fragment_home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_home)
+    }
+}
