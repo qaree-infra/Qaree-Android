1028fbe15fb32b281244ffeaa85b5ed688198c0d
Muhmmad Nabil <muhmmad.nabil.albadry@gmail.com>
2024-03-10 03:48:28 +0200

    Handle library data

--- app/src/main/java/com/muhmmad/qaree/ui/fragment/library/LibraryFragment.kt
+++ app/src/main/java/com/muhmmad/qaree/ui/fragment/library/LibraryFragment.kt
@@ -5,12 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.muhmmad.qaree.databinding.FragmentLibraryBinding
+import com.muhmmad.qaree.ui.activity.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class LibraryFragment : Fragment() {
     private val binding: FragmentLibraryBinding by lazy {
         FragmentLibraryBinding.inflate(layoutInflater)
     }
+    private val activity: MainActivity by lazy {
+        getActivity() as MainActivity
+    }
+
+    private val viewModel: LibraryViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +34,28 @@ class LibraryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+            //checkStatus
+            checkStatus()
+            viewModel.getLibrary()
+            btnAdd.setOnClickListener {
 
+            }
+        }
+    }
+
+    private fun checkStatus() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.state.collect {
+                if (it.isLoading) activity.showLoading()
+                else activity.dismissLoading()
+
+                if (it.error?.isNotEmpty() == true) activity.showError(it.error.toString())
+                if (it.libraryResponse != null) {
+
+                }
+
+                if (it.createShelfResponse != null) activity.showMessage(it.createShelfResponse.message)
+            }
         }
     }
 }
--- domain/src/main/java/com/muhmmad/domain/remote/RemoteDataSource.kt
+++ domain/src/main/java/com/muhmmad/domain/remote/RemoteDataSource.kt
@@ -8,7 +8,9 @@ import com.muhmmad.domain.model.ValidatePasswordOTPResponse
 import com.muhmmad.domain.model.BaseResponse
 import com.muhmmad.domain.model.BooksResponse
 import com.muhmmad.domain.model.CategoriesResponse
+import com.muhmmad.domain.model.LibraryResponse
 import com.muhmmad.domain.model.OffersResponse
+import com.muhmmad.domain.model.ShelfResponse
 
 interface RemoteDataSource {
     suspend fun login(email: String, pass: String): NetworkResponse<LoginResponse>
@@ -29,5 +31,9 @@ interface RemoteDataSource {
     suspend fun getTopAuthors(): NetworkResponse<AuthorsResponse>
     suspend fun getNewReleaseBooks(): NetworkResponse<BooksResponse>
     suspend fun getBestSellerBooks(): NetworkResponse<BooksResponse>
-    suspend fun getCategories():NetworkResponse<CategoriesResponse>
+    suspend fun getCategories(): NetworkResponse<CategoriesResponse>
+    suspend fun getLibrary(token: String): NetworkResponse<LibraryResponse>
+    suspend fun getShelfDetails(name: String,token: String): NetworkResponse<ShelfResponse>
+    suspend fun createShelf(name: String, token: String): NetworkResponse<BaseResponse>
+    suspend fun removeShelf(id: String, token: String): NetworkResponse<BaseResponse>
 }
