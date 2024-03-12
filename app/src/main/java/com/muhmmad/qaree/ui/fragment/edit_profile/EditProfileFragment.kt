b23121b0e307d35c053291ba36ffcd030445d823
Muhmmad Nabil <muhmmad.nabil.albadry@gmail.com>
2024-03-12 14:53:24 +0200

    new Screens

---
+++ app/src/main/java/com/muhmmad/qaree/ui/fragment/edit_profile/EditProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.muhmmad.qaree.ui.fragment.edit_profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muhmmad.qaree.databinding.FragmentEditProfileBinding
+
+class EditProfileFragment : Fragment() {
+    private val binding: FragmentEditProfileBinding by lazy {
+        FragmentEditProfileBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
+    }
+}
